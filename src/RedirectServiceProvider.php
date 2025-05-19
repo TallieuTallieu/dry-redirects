1b040f496dea8cf8115f02895da1a9a92ef2a00c
Korneel Eeckhout <korneel@pienter.tech>
2025-05-19 14:11:51 +0200

    Add automatic cleaning for redirect log

--- src/RedirectServiceProvider.php
+++ src/RedirectServiceProvider.php
@@ -18,6 +18,7 @@ use Tnt\Redirects\Contracts\RedirectPortalInterface;
 use Tnt\Redirects\Events\RouteWasHit;
 use Tnt\Redirects\Model\Redirect;
 use Tnt\Redirects\Model\RedirectLog;
+use Tnt\Redirects\Repository\RedirectLogRepository;
 use Tnt\Redirects\Revisions\CreateRedirectLogTable;
 use Tnt\Redirects\Revisions\CreateRedirectTable;
 
@@ -127,5 +128,9 @@ class RedirectServiceProvider extends ServiceProvider
             $redirectLog->redirect = $redirect;
             $redirectLog->save();
         });
+
+        if (RedirectLogRepository::count() > 20000) {
+            RedirectLogRepository::deleteAllBut(20000);
+        }
     }
 }
