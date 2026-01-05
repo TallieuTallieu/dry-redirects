3d333b6b7a61ee414de81d6945f1fc10182b4fd5
Korneel Eeckhout <korneel@pienter.tech>
2026-01-05 10:30:49 +0100

    refactor: migrate RedirectLogRepository to dry-dbi API

--- src/Repository/RedirectLogRepository.php
+++ src/Repository/RedirectLogRepository.php
@@ -13,13 +13,13 @@ class RedirectLogRepository
    */
   public static function count() {
     try {
-      $countQuery = Connection::get()->query('SELECT COUNT(*) as count FROM redirects_redirect_log');
+      $countQuery = Connection::get()->query_field('SELECT COUNT(*) as count FROM redirects_redirect_log');
 
       if (empty($countQuery)) {
         return 0;
       }
 
-      return $countQuery->current()['count'];
+      return $countQuery;
     } catch (\Exception $e) {
       Debug::log('Could not count redirect_log', $e);
       return 0;
@@ -34,7 +34,7 @@ class RedirectLogRepository
   */
   public static function deleteAllBut($count = 20000) {
     try {
-      $result = Connection::get()->query('SELECT id FROM redirects_redirect_log ORDER BY id DESC LIMIT 1 OFFSET ' . ($count - 1));
+      $result = Connection::get()->db_query('SELECT id FROM redirects_redirect_log ORDER BY id DESC LIMIT 1 OFFSET ' . ($count - 1));
 
       if (!$result->valid()) {
         return true;
